ce922a37f156af6f707754c78fc1c6a2a4642e38
jorgeluishdez <swatcat@gmail.com>
2017-05-18 12:25:51 -0500

    Fix for loop

--- delete_virtualwires.py
+++ delete_virtualwires.py
@@ -2,8 +2,8 @@
 
 import requests
 from requests.auth import HTTPBasicAuth
-for x in range (44,45):
-  url = "https://192.168.1.220/api/4.0/edges/edge-%d" %(x)
-  print url
+for x in range (3,10):
+  url = "https://192.168.1.220/api/2.0/vdn/virtualwires/virtualwire-%d" %(x)
+  #print url
   r = requests.delete(url, verify=False, auth=HTTPBasicAuth('admin', 'VMware1!VMware1!'))
   print r.status_code
